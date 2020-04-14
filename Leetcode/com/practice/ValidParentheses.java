abc8853f2230c2910f75d4aa864727f3703c871b
premagopu <premagopu7@gmail.com>
2020-04-14 17:07:59 -0400

    Valid Parentheses

---
+++ Leetcode/com/practice/ValidParentheses.java
@@ -0,0 +1,34 @@
+package com.practice;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+		System.out.println(isValid("(])"));
+	}
+
+	public static boolean isValid(String s) {
+		HashMap<Character, Character> hm = new HashMap<>();
+		hm.put(')', '(');
+		hm.put('}', '{');
+		hm.put(']', '[');
+		Stack<Character> st = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (hm.containsKey(c)) {
+				char top = st.isEmpty() ? '*' : st.pop();
+				if (top != hm.get(c)) {
+					return false;
+				}
+			} else {
+				st.push(c);
+			}
+		}
+
+		return st.isEmpty();
+
+	}
+
+}
